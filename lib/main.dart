fcea2fd51ca403ff43a4fb2b9157f65ccc99da95
yamankartal <yamankartal2@gmail.com>
2023-07-30 15:27:34 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,13 @@ import 'package:shopapp/features/tracking_feature/presentation/bloc/tracking_blo
 import 'core/app_feature/presentation/screens/initial_screen.dart';
 import 'injection.dart';
 
-void main()async {
-
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await init();
 
-    await init();
-
-  runApp(
-    MultiBlocProvider(providers: [
-        BlocProvider(create: (_)=>sl<OrdersBloc>()),
-        BlocProvider(create: (_)=>sl<TrackingBloc>()),
-
-  ], child: const InitialScreen() )
-  );
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider(create: (_) => sl<OrdersBloc>()),
+    BlocProvider(create: (_) => sl<TrackingBloc>()),
+  ], child: const InitialScreen()));
 }
-
-
